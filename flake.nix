b061e1df993e216aa79d2e4e761c96970dc5184d
Ben Magee <ben@quayside.dev>
2026-01-01 12:15:46 +0000

    build: create Nix flake including development environment
    chore: add Nix flake
    
    chore: add jujutsu to Flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A development environment for Quayside";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ...}:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          packages = [
+            eza
+            fd
+            bashInteractive
+            git
+            git-lfs
+            openssh
+            claude-code
+            jujutsu
+            nodejs
+          ];
+
+          shellHook = ''
+            source ${git}/share/bash-completion/completions/git-prompt.sh
+            export GIT_PS1_SHOWDIRTYSTATE=1
+            export GIT_PS1_SHOWSTASHSTATE=1
+            export GIT_PS1_SHOWUNTRACKEDFILES=1
+            export GIT_PS1_SHOWCOLORHINTS=1
+            PROMPT_COMMAND='PS1_CMD1=$(__git_ps1 " (%s)")'
+            PS1='\[\e[38;5;75m\](quayside)\[\e[0m\]''${PS1_CMD1} \w \$ '
+            alias ls=eza
+            alias find=fd
+          '';
+        };
+      }
+    );
+}
